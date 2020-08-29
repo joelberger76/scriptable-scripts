6275611157b8c916a608f5923ca3d81b59f92356
Joel Berger <joelberger@gmail.com>
2020-08-29 14:52:24 -0400

    Added ctags support

--- bin/Deploy Scriptable Scripts.sh
+++ bin/Deploy Scriptable Scripts.sh
@@ -2,3 +2,4 @@
 
 rsync -a --exclude .git/ ~/Code/scriptable-scripts/scripts/ ~/Library/Mobile\ Documents/iCloud\~dk\~simonbs\~Scriptable/Documents/ --delete
 
+ctags --excmd=number --tag-relative=no --fields=+a+m+n+S -f /Users/joel/Library/Application\ Support/BBEdit/Completion\ Data/JavaScript/scriptable-scripts.tags -R /Users/joel/Code/scriptable-scripts/scripts >/dev/null 2>&1
